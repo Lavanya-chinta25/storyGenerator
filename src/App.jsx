ffdd35cc1b493da2721212a63c9981915a0abbc2
lavanya <lavanyachinta6@gmail.com>
2024-12-13 14:37:47 +0530

    intial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from "./pages/home"
-import Mythos from "./pages/mythos"
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Navbar from './components/navbar'
-import Footer from './components/footer'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Mythos from './pages/mythos';
+import About from './pages/about';
+import Contact from './pages/contact';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
-       <Navbar/>
-       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/mythos" element={<Mythos/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/contact" element={<Contact/>}/>
+      <Navbar />
+      <Routes>
+        <Route path="/storyGenerator" element={<Home />} />
+        <Route path="/storyGenerator/mythos" element={<Mythos />} />
+        <Route path="/storyGenerator/about" element={<About />} />
+        <Route path="/storyGenerator/contact" element={<Contact />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from "react-router-dom"
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Home } from './pages/home.jsx';
+import { Mythos } from './pages/mythos.jsx';
+import { About } from './pages/about.jsx';
+import { Contact } from './pages/contact.jsx';
 
+// Define the routes
+const router = createBrowserRouter([
+  {
+    path: '/storyGenerator/',
+    element: <App />,
+    children: [
+      {
+        path: '/storyGenerator/',
+        element: <Home />,
+      },
+      {
+        path: '/storyGenerator/mythos',
+        element: <Mythos />,
+      },
+      {
+        path: '/storyGenerator/contact',
+        element: <Contact />,
+      },
+      {
+        path: '/storyGenerator/about',
+        element: <About />,
+      },
+    ],
+  },
+]);
+
+// Render the app
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>
-)
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
+
