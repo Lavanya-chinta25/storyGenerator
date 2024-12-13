7735f8623e3c823da881b258b42f846f398a92f8
lavanya <lavanyachinta6@gmail.com>
2024-12-13 11:04:47 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/wordWeave/',  // Correct base for GitHub Pages
+  base: '/storyGenerator/',  // Correct base for GitHub Pages
 });
 
